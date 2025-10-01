3616303485e8b8d22928a3975edc7181019e0ef3
Peter Boshra <Peter.Boshra@linkdev.com>
2025-10-01 21:55:10 +0400

    create task added but not working

--- Controllers/PMController.cs
+++ Controllers/PMController.cs
@@ -105,5 +105,46 @@ public class PMController (ProjectService _pservice) : Controller
         return View();
     }
 
-    
+    public IActionResult CreateTask()
+    {
+        //// Always pass a new Task instance with ProjectId
+        var task = new Jobick.Models.Task
+        {
+            ProjectId = 1,
+            StageName = "",
+            Task1 = "",
+            ImplementorDepartment = "",
+            ExpectedStartDate = DateTime.Today,
+            ExpectedEndDate = DateTime.Today.AddDays(1),
+            DoneRatio = 0
+        };
+        return View("CreateTask", task);
+    }
+
+    [HttpPost]
+    public IActionResult PostTask(Models.Task model)
+    {
+        if (!ModelState.IsValid)
+            return View("TaskDetails", model);
+
+        var project = _pservice.GetProjectList()
+           .FirstOrDefault(p => p.Id == model.ProjectId);
+       
+        if (model.Id == 0)
+        {
+            model.CreatedDate = DateTime.Now;
+            project?.Tasks.Add(model);
+        }
+        else
+        {
+            var taskremove = project?.Tasks.FirstOrDefault(t => t.Id == model.Id);
+            project?.Tasks.Remove(taskremove);
+            model.CreatedDate = DateTime.Now;
+            project?.Tasks.Add(model);
+        }
+
+
+        // Redirect to ProjectDetails using the ProjectId
+        return RedirectToAction("ProjectDetails", "PM", new { id = model.ProjectId });
+    }
 }
